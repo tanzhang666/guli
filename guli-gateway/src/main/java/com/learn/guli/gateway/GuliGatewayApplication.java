e93167ecd0ba89664d48a9d9c84ef028b8c4577f
tanzhang666 <18717735973@163.com>
2021-02-02 18:37:16 +0800

    整理pom

---
+++ guli-gateway/src/main/java/com/learn/guli/gateway/GuliGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.learn.guli.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuliGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuliGatewayApplication.class, args);
+    }
+
+}
